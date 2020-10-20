be5125b82887e36aeef9f4ece6c6ae77b0521552
Johannes Würbach <johannes.wuerbach@googlemail.com>
2020-10-20 23:09:07 +0200

    Mark as read in parallel

--- index.ts
+++ index.ts
@@ -1,26 +1,42 @@
 import { Octokit, RestEndpointMethodTypes } from "@octokit/rest";
+import { ActivityListNotificationsForAuthenticatedUserResponseData } from "@octokit/types"
+
+interface Notification {
+  id: string;
+  subject: {
+    url: string
+    title: string
+  }
+}
 
 const ignoredUsers = ['dependabot[bot]', 'dependabot-preview[bot]']
+
+const maybeMarkAsRead = async (octokit: Octokit, notification: Notification): Promise<void> => {
+  const prResp = await octokit.request(`GET ${notification.subject.url}`) as RestEndpointMethodTypes["pulls"]["get"]["response"]
+  const { data: pr } = prResp
+
+  if (!ignoredUsers.includes(pr.user.login)) {
+    return
+  }
+
+  console.log(`Marking as read: ${notification.subject.title} by ${pr.user.login}`)
+  await octokit.activity.markThreadAsRead({ thread_id: parseInt(notification.id, 10) })
+}
 
 const init = async () => {
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
   const options = octokit.activity.listNotificationsForAuthenticatedUser
 
   for await (const notificationsResponse of octokit.paginate.iterator(options)) {
+    const markAsReadPromises = []
     for (const notification of notificationsResponse.data) {
       if (notification.subject.type != 'PullRequest') {
         continue
       }
 
-      const pr = await octokit.request(`GET ${notification.subject.url}`) as RestEndpointMethodTypes["pulls"]["get"]["response"]
-
-      if (!ignoredUsers.includes(pr.data.user.login)) {
-        continue
-      }
-
-      console.log(`Marking as read: ${notification.subject.title} by ${pr.data.user.login}`)
-      await octokit.activity.markThreadAsRead({ thread_id: parseInt(notification.id, 10) })
+      markAsReadPromises.push(maybeMarkAsRead(octokit, notification))
     }
+    await Promise.all(markAsReadPromises)
   }
 }
 
