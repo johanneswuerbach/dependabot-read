194b839a6604010a767510e201c2ac00ca56a1a7
Johannes Würbach <johannes.wuerbach@googlemail.com>
2020-10-26 09:15:41 +0100

    fix: catch notification shifting response pages

--- index.ts
+++ index.ts
@@ -11,32 +11,41 @@ interface Notification {
 
 const ignoredUsers = ['dependabot[bot]', 'dependabot-preview[bot]']
 
-const maybeMarkAsRead = async (octokit: Octokit, notification: Notification): Promise<void> => {
+const maybeMarkAsRead = async (octokit: Octokit, notification: Notification): Promise<boolean> => {
   const prResp = await octokit.request(`GET ${notification.subject.url}`) as RestEndpointMethodTypes["pulls"]["get"]["response"]
   const { data: pr } = prResp
 
   if (!ignoredUsers.includes(pr.user.login)) {
-    return
+    return false
   }
 
   console.log(`Marking as read: ${notification.subject.title} by ${pr.user.login}`)
   await octokit.activity.markThreadAsRead({ thread_id: parseInt(notification.id, 10) })
+
+  return true
 }
 
 const init = async () => {
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
   const options = octokit.activity.listNotificationsForAuthenticatedUser
+  let rerun = true
 
-  for await (const notificationsResponse of octokit.paginate.iterator(options)) {
-    const markAsReadPromises = []
-    for (const notification of notificationsResponse.data) {
-      if (notification.subject.type != 'PullRequest') {
-        continue
+  while (rerun) {
+    rerun = false
+    for await (const notificationsResponse of octokit.paginate.iterator(options)) {
+      const markAsReadPromises = []
+      for (const notification of notificationsResponse.data) {
+        if (notification.subject.type != 'PullRequest') {
+          continue
+        }
+
+        markAsReadPromises.push(maybeMarkAsRead(octokit, notification))
       }
+      const result = await Promise.all(markAsReadPromises)
 
-      markAsReadPromises.push(maybeMarkAsRead(octokit, notification))
+      // We need to rerun as notifictions can shift pages while we mark others as read
+      rerun = !!result.find((read) => read)
     }
-    await Promise.all(markAsReadPromises)
   }
 }
 
